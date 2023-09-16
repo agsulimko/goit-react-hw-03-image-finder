c3f036ee4828646c64b0d7702e37091e123153fb
agsulimko <125898308+agsulimko@users.noreply.github.com>
2023-09-16 09:43:29 +0200

    s

--- src/components/ButtonLoad/ButtonLoad.jsx
+++ src/components/ButtonLoad/ButtonLoad.jsx
@@ -1,9 +1,12 @@
  import Button from '@mui/material/Button'
+ import css from './ButtonLoad.module.css'
 const ButtonLoad = ({ handleBtnLoad }) => {
     return (
+      <div className={css.btnLoder}>
       <Button variant="contained" onClick={handleBtnLoad} type="button">
         Load more
       </Button>
+      </div>
     );
   };
   
